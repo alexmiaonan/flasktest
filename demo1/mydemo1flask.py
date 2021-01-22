cc67f5ccbf12caa85890983d5d69495f968145e3
miao <gao@qq.com>
2021-01-22 09:25:59 +0800

    第一个flask项目

---
+++ demo1/mydemo1flask.py
@@ -0,0 +1,14 @@
+"""
+author:gcm
+c_date:2021/1/22 9:22
+u_date:2021/1/22 9:22
+reversion:1.0
+file_name:mydemo1flask
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+if __name__ == '__main__':
+	app.run()
